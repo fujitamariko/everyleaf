b6e38ef098947e7b2dd70f9d5da0a19beb8b1747
fujitamariko <fujyta.mrk@gmail.com>
2021-09-18 20:35:33 +0900

    検索ロジックをtaskモデルに入力

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,7 @@ class Task < ApplicationRecord
     validates :content, presence: true
 
     enum status: { 完了: "完了", 着手中: "着手中", 未着手: "未着手" }
+
+    scope :search_by_task_title, -> (search){where('title like ?', "%#{search}%" )}
+    scope :search_by_status, -> status { where(status: status) }
 end  
