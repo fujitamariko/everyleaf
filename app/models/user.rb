fe04d322e0d235f669f065e9ba58c1b932c04139
fujitamariko <fujyta.mrk@gmail.com>
2021-09-21 15:32:43 +0900

    ユーザの管理画面と権限実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,24 @@ class User < ApplicationRecord
     has_secure_password
     validates :password, length: { minimum: 6 }
     has_many :tasks
+    before_destroy :reject_destroy_admin
+    before_update :reject_update_admin
+
+    private
+    def reject_destroy_admin
+        if User.where(admin: true).count == 1
+          user = User.where(admin: true)
+          throw :abort if user[0] == self
+        end
+    end
+    
+    def reject_update_admin
+        if User.where(admin: true).count == 1 && admin == false
+          user = User.where(admin: true)
+          if user[0] == self
+            errors.add(:user, '更新にエラーがあります。現在あなたのみが管理人のため管理人から外れることはできません。')
+            throw :abort
+          end
+        end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,13 @@
+# User.create!(
+#     name: 'test1',
+#     email: 'test1@email.com',
+#     password: '12345678',
+#     password_confirmation: '12345678'
+# )
 User.create!(
-    name: 'test1',
-    email: 'test1@email.com',
+    name:  '管理者2',
+    email: 'xxx@email.com',
     password: '12345678',
     password_confirmation: '12345678',
+    admin: true
 )
