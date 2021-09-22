466d10c65112af06781988f38c85c1449d25b564
fujitamariko <fujyta.mrk@gmail.com>
2021-09-22 17:39:10 +0900

    ラベル機能の実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,10 +4,34 @@
 #     password: '12345678',
 #     password_confirmation: '12345678'
 # )
-User.create!(
-    name:  '管理者2',
-    email: 'xxx@email.com',
-    password: '12345678',
-    password_confirmation: '12345678',
-    admin: true
+# User.create!(
+#     name:  '管理者2',
+#     email: 'xxx@email.com',
+#     password: '12345678',
+#     password_confirmation: '12345678',
+#     admin: true
+# )
+
+Label.create!(
+    name: 'S.M. Entertainment'
+)
+
+Label.create!(
+    name: 'BIGHIT MUSIC'
+)
+
+Label.create!(
+    name: 'Pledis Entertainment'
+)
+
+Label.create!(
+    name: 'JYP Entertainment'
+)
+
+Label.create!(
+    name: 'YG Entertainment'
+)
+
+Label.create!(
+    name: 'その他'
 )
